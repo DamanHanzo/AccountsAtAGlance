e16822be07c656293daa354a16f0b5065073c103
Daman Singh <singhdamanpreet1234@gmail.com>
2017-01-31 13:32:13 -0500

    adding model classes to models project

---
+++ AccountsAtAGlance/AccountAtAGlance.Model/BrokerageAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountAtAGlance.Model
+{
+    public class BrokerageAccount
+    {
+        public BrokerageAccount()
+        {
+            //HasSet: A HashSet holds a set of objects, but in a way that it allows you to easily and quickly determine whether an object is already in the set or not.
+            //It does so by internally managing an array and storing the object using an index which is calculated from the hashcode of the object.
+            Positions = new HashSet<Position>();
+            Orders = new HashSet<Order>();
+        }
+
+        //Primitive properties
+        [Key]
+        public int Id { get; set; }
+        public string AccountNumber { get; set; }
+        public string AccountTitle { get; set; }
+        public decimal Total { get; set; }
+        public decimal MarginBalance { get; set; }
+        public bool IsRetirement { get; set; }
+        public int CustomerId { get; set; }
+        public decimal CashTotal { get; set; }
+        public decimal PositionsTotal { get; set; }
+
+        //Navigation properties
+       public ICollection<Position> Positions { get; set; }
+       public ICollection<Order> Orders { get; set; }
+       public WatchList WatchList { get; set; } 
+    }
+}
---
+++ AccountsAtAGlance/AccountAtAGlance.Model/Position.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountAtAGlance.Model
+{
+    public class Position
+    {
+        //Primitive properties
+        public int Id { get; set; }
+        public int SecurityId { get; set; }
+        public decimal Shares { get; set; }
+        public decimal Total { get; set; }
+        public int BrokerageAccountId { get; set; }
+
+        //Navigation properties
+        //virtual modifier: The virtual modifier tells the compiler that when any class derived from class A is used, an override method should be called.
+        public virtual Security Security { get; set; }
+    }
+}
