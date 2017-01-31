73897fb038e9926a0d7eb0084b4a7332f92f460d
Daman Singh <singhdamanpreet1234@gmail.com>
2017-01-31 15:18:03 -0500

    adding dbcontext file to repository project

---
+++ AccountsAtAGlance/AccountsAtAGlance.Repository/AccountAtAGlance.cs
@@ -0,0 +1,41 @@
+﻿using AccountAtAGlance.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountsAtAGlance.Repository
+{
+    public class AccountAtAGlance : DbContext
+    {
+        public DbSet<BrokerageAccount> BrokerageAccounts { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Exchange> Exchanges { get; set; }
+        public DbSet<MarketIndex> MarketIndexes { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderType> OrderTypes { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Security> Securities { get; set; }
+        public DbSet<MutualFund> MutualFunds { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<WatchList> WatchLists { get; set; }
+
+        //gives access to model builder
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Security>().ToTable("Security");
+            modelBuilder.Entity<Stock>().ToTable("Securities_Stock");
+            modelBuilder.Entity<MutualFund>().ToTable("Securities_MutualFund");
+
+            modelBuilder.Entity<WatchList>()
+                .HasMany(d => d.Securities).WithMany()
+                .Map(map => map.ToTable("WatchListSecurity")
+                .MapRightKey("SecurityId")
+                .MapLeftKey("WatchListId"));
+
+        }
+    }
+}
