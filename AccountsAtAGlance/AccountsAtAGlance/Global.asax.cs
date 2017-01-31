f951a34291b5dab87f3ca4aa54b38e5e4375543e
Daman Singh <singhdamanpreet1234@gmail.com>
2017-01-31 11:55:42 -0500

    adding entity framework library to repository project

--- AccountsAtAGlance/AccountsAtAGlance/Global.asax.cs
+++ AccountsAtAGlance/AccountsAtAGlance/Global.asax.cs
@@ -13,9 +13,9 @@ namespace AccountsAtAGlance
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            //RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
--- AccountsAtAGlance/AccountsAtAGlance/Startup.cs
+++ AccountsAtAGlance/AccountsAtAGlance/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Owin;
+﻿using System;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(AccountsAtAGlance.Startup))]
@@ -9,6 +10,11 @@ namespace AccountsAtAGlance
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+        }
+
+        private void ConfigureAuth(IAppBuilder app)
+        {
+            throw new NotImplementedException();
         }
     }
 }
