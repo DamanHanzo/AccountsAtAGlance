c44016f84199533d5b0b77cf5de09caff5f19780
Daman Singh <singhdamanpreet1234@gmail.com>
2017-02-03 10:25:20 -0500

    adding controllers and updating interface repo classes

---
+++ AccountsAtAGlance/AccountsAtAGlance/Controllers/DataServiceController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AccountAtAGlance.Repository;
+
+namespace AccountsAtAGlance.Controllers
+{
+    public class DataServiceController : Controller
+    {
+        IAccountRepository _AccountRepository;
+        ISecurityRepository _SecurityRepository;
+        IMarketsAndNewsRepository _MarketRepository;
+
+        // GET: DataService
+        public DataServiceController() : this(null, null, null)
+        {
+                
+        }
+
+        public DataServiceController(IAccountRepository acctRepo, ISecurityRepository secRepo, IMarketsAndNewsRepository marketRepo)
+        {
+            _AccountRepository = acctRepo ?? new AccountRepository();
+            _SecurityRepository = secRepo;
+            _MarketRepository = marketRepo;
+        }
+        public ActionResult GetAccount(string acctNumber)
+        {
+
+
+            return null;
+        }
+
+        public ActionResult GetQuote(string symbol)
+        {
+            return null;
+        }
+
+        public ActionResult GetMarketIndexes()
+        {
+            return null;
+        }
+
+        public ActionResult GetTickerQuotes()
+        {
+            return null;
+        }
+    }
+}
